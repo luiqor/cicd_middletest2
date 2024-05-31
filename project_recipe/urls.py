b91845b30a63b17b7540e55f2aaeaa28858eefc1
luiqor <vohnenna@gmail.com>
2024-05-31 17:22:55 +0300

    Add category_detail view

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from recipe.views import main
+from recipe.views import main, category_detail
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("", main, name="main")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", main, name="main"),
+    path("category/<str:category_name>", category_detail, name="category_detail"),
+]
--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from .models import Recipe
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, Category
 
 
 def main(request):
     random_recipies = Recipe.objects.all().order_by("?")[:10]
 
     return render(request, "main.html", context={"recipes": random_recipies})
+
+
+def category_detail(request, category_name):
+    category = get_object_or_404(Category, name=category_name)
+    recipes = category.categories.all()
+    context = {
+        "category": category,
+        "recipes": recipes,
+    }
+    return render(request, "category_detail.html", context)
