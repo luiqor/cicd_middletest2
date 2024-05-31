7dc224379120cb39161d4eb30119b42693fd9a87
luiqor <vohnenna@gmail.com>
2024-05-31 17:39:27 +0300

    Add test for maximum number of items in main view

--- tests/test_recipe_main.py
+++ tests/test_recipe_main.py
@@ -6,3 +6,10 @@ from django.urls import reverse
 def test_main_view_status_code(client):
     response = client.get(reverse("main"))
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_main_10items_max(client, recipes):
+    response = client.get("/")
+
+    assert len(response.context["recipes"]) <= 10
