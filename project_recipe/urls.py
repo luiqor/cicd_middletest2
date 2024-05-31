e325b475cf77729c6f538f6a27f8ca7a9c27e2e9
luiqor <vohnenna@gmail.com>
2024-05-31 17:07:43 +0300

    Add main view

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from recipe.views import main
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", main, name="main")]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,8 @@
+from django.shortcuts import render
+from .models import Recipe
 
+
+def main(request):
+    random_recipies = Recipe.objects.all().order_by("?")[:10]
+
+    return render(request, "main.html", context={"recipes": random_recipies})
